dd9f1f17a7e3770149235bf22a1fe36663dbdf2a
Karl Brightman <karl@karlbright.org>
2012-06-08 08:48:10 +0800

    Added basic README, license and cleaned up package.json. Also added expect and assert from chai

--- scripts/SpecRunner.js
+++ scripts/SpecRunner.js
@@ -1,3 +1,4 @@
+// Configure RequireJS
 require.config({
   baseUrl:'src/',
   urlArgs: "v="+(new Date()).getTime()
@@ -5,12 +6,19 @@ require.config({
 
 // Require libraries
 require(['require', 'vendor/chai', 'vendor/mocha'], function(require,chai){
-  // Setup Chai & Mocha
+
+  // Chai
+  assert = chai.assert;
   should = chai.should();
+  expect = chai.expect;
+
+  // Mocha
   mocha.setup('bdd');
+
   // Require base tests before starting
   require(['spec/person'], function(person){
     // Start runner
     mocha.run();
   });
+
 });
